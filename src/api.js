12caba3aa8318a6d7f02eea8afc7a8c38cde2dfc
tnhoang <tnhoang088@gmail.com>
2021-08-03 16:25:11 +0700

    update fake calling api

--- src/api.js
+++ src/api.js
@@ -1,22 +1,24 @@
 import axiosClient from "./axiosClient";
 import LocalStorageService from "./localStorageService";
 
+const delay = async (time) => {
+  await new Promise((res) => setTimeout(res, time));
+};
+
 export const getAllBooks = async () => {
-  const response = await axiosClient.get(`${process.env.REACT_APP_API_SERVER}`);
-
-  if (response.status !== 200) {
-    throw new Error("Something went wrong.");
-  }
-
-  return response.data;
+  await delay(1000);
+  return [1, 2, 3];
 };
 
 export const authentication = async () => {
   const [username, password] = ["qwe", "password"];
-  const response = await axiosClient.post("http://0.0.0.0:8002/login", {
-    username: username,
-    password: password,
-  });
+  const response = await axiosClient.post(
+    `${process.env.REACT_APP_API_SERVER}/login`,
+    {
+      username: username,
+      password: password,
+    }
+  );
 
   const obj = {
     access_token: response.data.access_token,
@@ -24,68 +26,4 @@ export const authentication = async () => {
   };
 
   LocalStorageService.setToken(obj);
-};
-
-export const getBook = async ({ queryKey }) => {
-  /* eslint-disable no-unused-vars */
-  const [_key, { id }] = queryKey;
-  const response = await fetch(
-    `${process.env.REACT_APP_API_SERVER}/books/${id}`
-  );
-
-  if (!response.ok) {
-    throw new Error(response.json().message);
-  }
-
-  return response.json();
-};
-
-export const updateBook = async ({ id, ...data }) => {
-  const response = await fetch(
-    `${process.env.REACT_APP_API_SERVER}/books/${id}`,
-    {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    }
-  );
-
-  if (!response.ok) {
-    throw new Error(response.json().message);
-  }
-
-  return response.json();
-};
-
-export const createBook = async ({ ...data }) => {
-  const response = await fetch(`${process.env.REACT_APP_API_SERVER}/books/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-
-  if (!response.ok) {
-    throw new Error(response.json().message);
-  }
-
-  return response.json();
-};
-
-export const removeBook = async (id) => {
-  const response = await fetch(
-    `${process.env.REACT_APP_API_SERVER}/books/${id}`,
-    {
-      method: "DELETE",
-    }
-  );
-
-  if (!response.ok) {
-    throw new Error(response.json().message);
-  }
-
-  return true;
 };
